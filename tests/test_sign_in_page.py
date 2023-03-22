24464c5294a0d06a6f2340c032b743ae7006cf28
Sangeetha <89851961+sangeeritu@users.noreply.github.com>
2023-03-22 17:27:08 +0100

    Update test_sign_in_page.py

--- tests/test_sign_in_page.py
+++ tests/test_sign_in_page.py
@@ -4,10 +4,6 @@ from pages.main_page import *
 from utils.test_cases import test_cases
 
 
-# I am using python unittest for asserting cases.
-# In this module, there should be test cases.
-# If you want to run it, you should type: python <module-name.py>
-
 class TestSignInPage(BaseTest):
 
     def test_page_load(self):
